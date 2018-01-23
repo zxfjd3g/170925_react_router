8749e16ae17dcf827476cd5e6a15b02b68981384
zxfjd3g <258147149@qq.com>
2018-01-23 10:55:11 +0800

    init app

---
+++ src/components/app.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import {NavLink, Switch, Route, Redirect} from 'react-router-dom'
+
+import About from '../views/about'
+import Home from '../views/home'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header"><h2>React Router Demo</h2></div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              <NavLink className='list-group-item' to='/about'>About</NavLink>
+              <NavLink className='list-group-item' to='/home'>Home</NavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                <Switch>
+                  <Route path='/about' component={About}/>
+                  <Route path='/home' component={Home}/>
+                  <Redirect to='/about'/>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
