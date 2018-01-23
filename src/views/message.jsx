5505a5f957cf8098a786c2e77f9dade95979684d
zxfjd3g <258147149@qq.com>
2018-01-23 14:22:27 +0800

    路由跳转

--- src/views/message.jsx
+++ src/views/message.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {Route} from 'react-router-dom'
 
 import MessageDetail from './message-detail'
+import MyNavLink from "../components/my-nav-link";
 
 export default class Message extends Component {
 
@@ -23,6 +24,26 @@ export default class Message extends Component {
     }, 1000)
   }
 
+  showDetail = (id) => {
+    this.props.history.push(`/home/message/messagedetail/${id}`)
+  }
+  showDetail2 = (id) => {
+    this.props.history.replace(`/home/message/messagedetail/${id}`)
+  }
+
+  back = () => {
+    this.props.history.goBack()
+  }
+
+  forward = () => {
+    this.props.history.goForward()
+  }
+
+  reqPage = () => {
+    // 通过js进行页面跳转
+    window.location = 'http://www.baidu.com'
+  }
+
   render () {
     return (
       <div>
@@ -30,15 +51,23 @@ export default class Message extends Component {
           {
             this.state.messages.map((m,index) => (
               <li key={index}>
-                <a href={`/home/message/messagedetail/${m.id}`}>{m.title}</a>
+                <MyNavLink to={`/home/message/messagedetail/${m.id}`}>{m.title}</MyNavLink>
+                &nbsp;&nbsp; <button onClick={() => {this.showDetail(m.id)}}>push()查看</button>
+                &nbsp;&nbsp; <button onClick={() => {this.showDetail2(m.id)}}>replace()查看</button>
               </li>
             ))
           }
         </ul>
+        <p>
+          <button onClick={this.back}>回退</button>
+          <button onClick={this.forward}>前进</button>
+        </p>
+
+        <p>
+          <button onClick={this.reqPage}>页面跳转</button>
+        </p>
         <Route path='/home/message/messagedetail/:id' component={MessageDetail}/>
       </div>
-
-
     )
   }
 }
